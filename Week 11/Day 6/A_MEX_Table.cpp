1a7f10f323747429880264de114458e592146f88
bandhansaha2 <bandhansaha2@gmail.com>
2025-01-11 21:49:13 +0600

    Week 11 Day 6

---
+++ Week 11/Day 6/A_MEX_Table.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while(t--) {
+        int n,m;
+        cin >> n >>  m;
+        cout << max(n,m) + 1 << endl;
+    }
+
+    return 0;
+}
