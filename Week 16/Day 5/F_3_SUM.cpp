57962ae12ebd6952d4c6e9db32789524dde06e1c
bandhansaha2 <bandhansaha2@gmail.com>
2025-03-03 23:40:33 +0600

    Week 16 Day 5

--- Week 16/Day 5/F_3_SUM.cpp
+++ Week 16/Day 5/F_3_SUM.cpp
@@ -14,49 +14,57 @@ int main()
     int t; cin >> t;
     while(t--) {
         ll n; cin >> n;
-        vector <int> v(n),d(n);
+        vector <int> v(n);
 
         for(int i=0; i<n; i++) {
             cin >> v[i];
         }
 
-        map <int,int> mp;
+        vector<int> d,freq(11,0);
         for(int i=0; i<n; i++) {
-            d[i] = v[i] % 10;
-            mp[d[i]]++;
+            if(freq[v[i] % 10] == 0) d.push_back(v[i]%10) ;
+            freq[v[i]%10]++;
         }
 
         bool flag = false;
-        n = mp.size();
-        if(mp[1]>=3) flag = true;
-        for(int i=0; i<n; i++) {
-            if(mp[i]>=2) {
-                for(int j=0; j<n; j++) {
-                    if(mp[i] == mp[j]) continue;
-                    if((2*(mp[i] + mp[j])) % 10 == 3) {
-                        flag = true;
-                        // break;
-                    }
-                }
-            }
-        }
-        // if(!flag) {
+        n = d.size();
+
+        if(freq[1]>=3) flag = true;
+
+        if(!flag) {
             for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    for(int k=0; k<n; k++) {
-                        if(i!=j && j!=k && k!=i && (mp[i] + mp[j] + mp[k]) % 10 == 3) {
+                if(flag) break;
+                if(freq[d[i]] >= 2) {
+                    for(int j=0; j<n; j++) {
+                        if(d[i] == d[j]) continue;
+                        if(((2*d[i]) + d[j]) % 10 == 3) {
                             flag = true;
-                            // break;
+                            break;
                         }
                     }
                 }
             }
-        // }
+        }
+
+        if(!flag) {
+            for(int i=0; i<n; i++) {
+                if(flag) break;
+                for(int j=0; j<n; j++) {
+                    if(flag) break;
+                    for(int k=0; k<n; k++) {
+                        if(i!=j && j!=k && i!=k && (d[i] + d[j] + d[k]) % 10 == 3) {
+                            flag = true;
+                            break;
+                        }
+                        
+                    }
+                }
+            }
+        }
 
         if(flag) Yes;
         else No;
 
-        //cout << endl;
     }
 
     return 0;
